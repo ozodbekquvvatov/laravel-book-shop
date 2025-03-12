e4de7e51a34cb502711223a8ecb150ffe8e54f17
Ozodbek Quvvatov <ozodbekquvvatov22@gmail.com>
2025-03-12 18:15:32 +0500

    project

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePoductRequest;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return "Ma'lumotlar muvaffaqiyatli qo'shildi!";
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePoductRequest $request)
+    {
+        Product::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'price' => $request->price,
+            'discount' => $request->discount ?? 0,
+        ]);
+
+        return redirect()->route('products.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ resources/views/create.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Create Item</title>
+</head>
+<body>
+    <h1>Create New Item</h1>
+   
+
+
+<form action="{{ route('products.store') }}" method="POST">
+    @csrf
+    @error('title')
+    <p style="color: red">{{ $message }}</p>        
+        @enderror
+    <label for="title">Nomi:</label>
+    <input type="text" name="title" ><br>
+
+    @error('description')
+    <p style="color: red">{{ $message }}</p>
+        
+    @enderror
+    <label for="description">Tavsif:</label>
+    <textarea name="description"></textarea><br>
+
+
+    @error('price')
+    <p style="color: red">{{ $message }}</p>
+        
+    @enderror
+    <label for="price">Narxi:</label>
+    <input type="text" name="price" step="0.01" ><br>
+    @error('discount')
+    <p style="color: red">{{ $message }}</p>
+        
+    @enderror
+    <label for="discount">Chegirma (%):</label>
+    <input type="text" name="discount" step="0.01" min="0" max="100"><br>
+
+    <button type="submit">Saqlash</button>
+</form>
+
+</div>
+</body>
+</html>
